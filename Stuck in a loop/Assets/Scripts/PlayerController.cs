2b58becc96ca550c2356baca3569d8e11638b254
mungobulge <sebastienbaby@hotmail.com>
2020-10-02 20:50:21 -0400

    First Idea Space Loop

---
+++ Stuck in a loop/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private float thrust = 10.0f;
+
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.AddForce(transform.right * thrust);
+    }
+}
