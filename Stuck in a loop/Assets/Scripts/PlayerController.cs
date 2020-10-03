0cfdce14f469258af7bdcafd6f558ffd8a6b37dd
mungobulge <sebastienbaby@hotmail.com>
2020-10-02 22:48:06 -0400

    Gravity and attraction

--- Stuck in a loop/Assets/Scripts/PlayerController.cs
+++ Stuck in a loop/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float thrust = 1.0f;
+
     private Rigidbody2D rb2D;
-    private float thrust = 10.0f;
 
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
-        transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-    }
-
-    void FixedUpdate()
-    {
         rb2D.AddForce(transform.right * thrust);
     }
 }
---
+++ Stuck in a loop/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform reciever;
+
+    private bool playerIsOverlapping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+
+            float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+            rotationDiff += 180;
+            player.Rotate(Vector3.up, rotationDiff);
+
+            Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+            player.position = reciever.position + positionOffset;
+
+            playerIsOverlapping = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+}
